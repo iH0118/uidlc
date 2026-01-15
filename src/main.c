4a1311ba3001d7da2e43cd6e1e3551fa9e3c3b62
iH0118 <66252845+iH0118@users.noreply.github.com>
2026-01-15 06:55:32 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+
+
+
+
+const char *helpmsg = "uidlc - the uidl bytecode compiler\n"
+    "usage:\nuidlc [-h] [-o <outfile>] <infile>\n";
+
+const char *uib_header = "";
+
+
+
+int 
+main (
+    int argc,
+    char **argv
+)
+{
+    char *input_filename;
+    char *output_filename = "out.uib";
+
+    int c;
+
+    while ((c = getopt (argc, argv, "ho:")) != -1) switch (c) 
+    {
+        case 'h':
+        fputs(helpmsg, stderr);
+        return 0;
+        break;
+
+        case 'o':
+        output_filename = optarg;
+        break;
+
+        case '?':
+        fputs(helpmsg, stderr);
+        return 1;
+        
+        default:
+        abort();
+    }
+
+    if (optind >= argc)
+    {
+        fputs("eror: missing input file\n", stderr);
+        return 1;
+    }
+
+    input_filename = argv[optind++];
+
+    if (optind < argc)
+    {
+        fputs("error: too many arguments\n", stderr);
+        return 1;
+    }
+
+    FILE *infile = fopen(input_filename, "w");
+
+    if (!infile)
+    {
+        fputs("error: input file not found", stderr);
+        return 1;
+    }
+
+
+
+
+
+
+
+}
